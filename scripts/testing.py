808e5f42f4139ebdaabb2090e62ab5cc0c975bb2
GigantPro <pochtagigantpro@gmail.com>
2024-11-10 22:37:36 +0300

    Continue of change version

--- scripts/testing.py
+++ scripts/testing.py
@@ -4,9 +4,3 @@ from os import system
 
 def start_testing() -> None:  # ignore: ANN201
     system('pytest')
-
-    platform = sys.platform
-    if platform == 'win32':
-        system('rmdir /a test_saves')
-    else:
-        system('rm -rf test_saves')
